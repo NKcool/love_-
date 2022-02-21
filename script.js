b2485beed56292ff8c123fdc9be9e8154687dc72
nk cool <nkcool3956@gmail.com>
2022-02-21 19:19:20 +0530

    alldone

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Hello friend  love_calculator welcomes you press ok to continue !")
+
+var person_1 = prompt("what is your name dear ! ");
+var parson_2 = prompt("what is your crush name or anyone you want to be you humpy-dumpy .")
+
+var schore = Math.random();
+
+schore = Math.floor((schore * 100))+1;
+
+console.log(person_1+" Your love schore with "+parson_2 +" is ♥♥♥ "+ schore );
+
+alert(person_1+" Your love schore with "+parson_2 +" is "+ schore );
+alert ("Note ! This schore calculation based on the position of planets. so if you try again the schore will be different ! :)")
